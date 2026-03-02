daf5d5ff99c0bc2e9e314f54d413f207d26eff45
Alex Loewenthal <57980018+alexloew@users.noreply.github.com>
2026-03-01 19:12:57 -0500

    Clean up comments in tpm.nix
    Removed comments regarding tctiEnvironment and metatron conflicts.

--- system/tpm.nix
+++ system/tpm.nix
@@ -8,9 +8,6 @@
   security.tpm2 = {
     enable = true;
     pkcs11.enable = true;              # PKCS#11 interface
-    # Do NOT enable tctiEnvironment: it sets TPM2TOOLS_TCTI=device:/dev/tpmrm0
-    # which metatron-cli cannot parse (nflxaccess-go TCTI bug). Leave unset so
-    # metatron falls back to its hardcoded /dev/tpmrm0 default.
   };
 
   # Grant tss group access to /dev/tpm0
@@ -18,10 +15,6 @@
     KERNEL=="tpm0", MODE="0660", GROUP="tss"
   '';
 
-  # After running `metatron enroll`, remove conflicting files before nixos-rebuild switch:
-  #   rm ~/.config/systemd/user/metatron-agent.service
-  #   rm ~/.config/systemd/user/default.target.wants/metatron-agent.service
-  # See: Netflix/Issues/nix-findings/metatron-enroll-homemgr-conflict.md
   environment.systemPackages = with pkgs; [
     tpm2-tools
   ];
